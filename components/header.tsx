ad4e35999898f680d7e950fd9e21a9b1a00db142
Chad Derya <chad@perlego.com>
2023-08-20 19:34:09 +0100

    cloned next-auth example and made it work with github and email log in. removed server tab as it was erroring. integrated supabase.

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,13 @@
-import Link from "next/link"
-import { signIn, signOut, useSession } from "next-auth/react"
-import styles from "./header.module.css"
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+import styles from "./header.module.css";
 
 // The approach used in this component shows how to build a sign in and sign out
 // component that works on pages which support both client and server side
 // rendering, and avoids any flash incorrect content on initial page load.
 export default function Header() {
-  const { data: session, status } = useSession()
-  const loading = status === "loading"
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
 
   return (
     <header>
@@ -29,8 +29,8 @@ export default function Header() {
                 href={`/api/auth/signin`}
                 className={styles.buttonPrimary}
                 onClick={(e) => {
-                  e.preventDefault()
-                  signIn()
+                  e.preventDefault();
+                  signIn();
                 }}
               >
                 Sign in
@@ -54,8 +54,8 @@ export default function Header() {
                 href={`/api/auth/signout`}
                 className={styles.button}
                 onClick={(e) => {
-                  e.preventDefault()
-                  signOut()
+                  e.preventDefault();
+                  signOut();
                 }}
               >
                 Sign out
@@ -73,9 +73,6 @@ export default function Header() {
             <Link href="/client">Client</Link>
           </li>
           <li className={styles.navItem}>
-            <Link href="/server">Server</Link>
-          </li>
-          <li className={styles.navItem}>
             <Link href="/protected">Protected</Link>
           </li>
           <li className={styles.navItem}>
@@ -90,5 +87,5 @@ export default function Header() {
         </ul>
       </nav>
     </header>
-  )
+  );
 }
